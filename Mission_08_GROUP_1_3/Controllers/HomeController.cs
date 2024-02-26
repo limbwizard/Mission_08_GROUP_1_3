3d0113a023cac58c75726a6ec946b4cd2f696452
coolwhipki <kirajanegirl@gmail.com>
2024-02-26 10:16:36 -0700

    deleted things we didn't need

--- Mission_08_GROUP_1_3/Controllers/HomeController.cs
+++ Mission_08_GROUP_1_3/Controllers/HomeController.cs
@@ -1,32 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using Mission_08_GROUP_1_3.Models;
+/*using Mission_08_GROUP_1_3.Models;*/
 using System.Diagnostics;
 
 namespace Mission_08_GROUP_1_3.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
